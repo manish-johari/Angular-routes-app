f0f6e3954eb64f208c7cfc6d9270b9f2220e9115
Manish Johari <manish.johari@kiwitech.com>
2017-06-05 17:50:49 +0530

    Create app.router.ts and include it in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { routes } from './app.router';
 import { AppComponent } from './app.component';
 import { ServicesComponent } from './services/services.component';
 import { AboutComponent } from './about/about.component';
